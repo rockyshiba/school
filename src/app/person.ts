7f8b940eabbace788a2585d471ad1a40cb1be462
LeeS <leejsitu@gmail.com>
2018-06-02 13:32:11 -0400

    Added Person class

---
+++ src/app/person.ts
@@ -0,0 +1,21 @@
+export class Person {
+    id: number;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+
+    getAge(): number{
+        
+        let currentDate = new Date();
+        let age: number = currentDate.getFullYear() - this.dateOfBirth.getFullYear();
+        
+        if(
+            this.dateOfBirth.getDate() > currentDate.getDate() &&
+            this.dateOfBirth.getMonth() > currentDate.getMonth()
+        ){
+            age--;
+        }
+
+        return age;
+    }
+}
